7b0d78255628914fc7b5c588bf654bfc50105b5a
Rio Flores <rio517@gmail.com>
2014-12-08 16:06:13 -0500

    minor documentation update

--- _plugins/custom_filters.rb
+++ _plugins/custom_filters.rb
@@ -1,8 +1,10 @@
 module Jekyll
   module MyFilters
+    # Github doesn't allow plugins to execute (safemode). This plugin would require an infrastructure change to be reliable.
+    #
     # Gets Md5 contents of file (assumed to be using the full path)
     # and returns the hash to be appended to any CSS file
-    # when used with css file, automatically makes sure that deployed CSS 
+    # when used with css file, automatically makes sure that deployed CSS
     # is cachebust every time the file changes
     def md5_cache_bust(file_name)
       require 'digest/md5'
